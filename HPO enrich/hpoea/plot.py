d9dd02b2436234bab521aeee1338ec5e61af5cf5
nanguage <nanguage@yahoo.com>
2019-03-28 12:54:13 +0800

    update

--- HPO enrich/hpoea/plot.py
+++ HPO enrich/hpoea/plot.py
@@ -1,31 +1,8 @@
 from bokeh.plotting import figure
-
-def bar_plot(enrich_table, figsize, show_items=10):
-    """ Draw the enrichment bar chart.
-
-    Args
-    ----
-    enrich_table : pandas.DataFrame
-        Enrichment result table, see `hpoea.enrich.GSEA`.
-    figsize : tuple, optional
-        The size of output figure, (width, height).
-    show_items : int, optional
-        Number of items to show. default 10.
-    """
-    if figsize:
-        width, height = figsize
-    else:  # set default figsize
-        grid_height = 70
-        height = grid_height * show_items
-        width = 500
-    p = figure(plot_width=width, plot_height=height)
-
-    df = enrich_table.iloc[:show_items, :]
-    p.circle()
-    return p
+from bokeh.models import ColumnDataSource, HoverTool
 
 
-def dot_plot(enrich_table, figsize=None, show_items=10):
+def dot_plot(enrich_table, figsize=None, show_items=10, x="gene_ratio", **kwargs):
     """ Draw the enrichment dot chart.
 
     Args
@@ -36,15 +13,43 @@ def dot_plot(enrich_table, figsize=None, show_items=10):
         The size of output figure, (width, height).
     show_items : int, optional
         Number of items to show. default 10.
+    x : str, optional
+        The x-axis coresponding value.
+        ['gene_ratio' | 'background_ratio' | 'odd_ratio' | 'pvalue' | 'padj']
     """
     if figsize:
         width, height = figsize
     else:  # set default figsize
-        grid_height = 70
+        grid_height = 50
         height = grid_height * show_items
         width = 500
-    p = figure(plot_width=width, plot_height=height)
 
-    df = enrich_table.iloc[:show_items, :]
-    p.circle()
+    if x in ['pvalue', 'padj']:
+        df = enrich_table.sort_values(by=x, ascending=True)
+    else:
+        df = enrich_table.sort_values(by=x, ascending=False)
+    df = df.iloc[:show_items, :]
+
+    source = ColumnDataSource(data=df)
+    p = figure(plot_width=width, plot_height=height,
+               y_range=list(df.HPO_term_name))
+
+    kwargs.setdefault("color", "navy")
+    kwargs.setdefault("size", 10)
+    kwargs.setdefault("alpha", 0.6)
+    p.circle(x=x, y='HPO_term_name',
+             source=source,
+             **kwargs)
+
+    p.add_tools(HoverTool(
+        tooltips=[
+            ( 'HPO_term_ID', '@HPO_term_ID' ),
+            ( 'gene_ratio',   '@gene_ratio{%0.2f}' ),
+            ( 'background_ratio',   '@background_ratio{%0.2f}' ),
+            ( 'odd_ratio',   '@odd_ratio{0.00}' ),
+            ( 'pvalue', '@pvalue' ),
+            ( 'padj', '@padj' )
+        ]
+    ))
+
     return p
